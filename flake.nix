d899a939278b8b0d444ae6d673226c494a85feb5
Andreas Grønlien <andreas@gronlien.no>
2026-04-17 22:57:48 +0200

    Initial NixOS flake configuration

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
